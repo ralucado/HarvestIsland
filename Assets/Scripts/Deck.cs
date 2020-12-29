5f8f9a2f823cf7216e1ea59cbe6f9c35a523c711
Ralu <ralucris.v@gmail.com>
2020-12-29 11:22:24 +0100

    Initial commit

---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    public GameObject cardPrefab;
+    public List<GameObject> cards;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Instantiate(cardPrefab, new Vector3(-1, 1, 0), Quaternion.identity);
+    }
+}
